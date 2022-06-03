ea47501f8f639253c51268820b23afae7430c4cf
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-03 13:34:53 +0530

    Assignment I

---
+++ block-BNaacv/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://localhost:27017/sample'),
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : 'Connected to database');
+  };
+
+var app = express();
+
+app.use(express.json());
+
+// setup view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// routing middleware
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  res.send('Page Not Found');
+});
+
+app.listen(3000, () => {
+  console.log('server is listening on port 3k');
+});
